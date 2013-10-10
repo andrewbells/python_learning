1e644a83529d8df8fd79e20edd2066b4e73f1b57
Andrey K <andkolok@gmail.com>
2013-10-10 21:07:08 +0400

    table feature added

--- mephi/divisors.py
+++ mephi/divisors.py
@@ -1,4 +1,4 @@
-
+from prettytable import PrettyTable
 
 def main():
     num_1 = int(input('add first number: '))
@@ -8,28 +8,41 @@ def main():
     
 
 def find_divisors(a, b):
+    t = PrettyTable(["Number", "Divisors", "no Divisors"])
+    t.padding_width = 1
     divisors_dict = {}
     num_divisors_dict = {}
     for x in range(a, b + 1):
         num_divisors = 0
         divisors_list = []
-        print('number %d' %x)
+        t.add_row([x, '-', '-'])
+        #print('number %d' %x)
         for y in range(2, x + 1):
             if x % y == 0 and x != y:
                 num_divisors += 1
                 divisors_list.append(y)
                 divisors_dict[x] = divisors_list
-                
-                print('has divisor %d' % y)
+                #print('has divisor %d' % y)
             
             else:
                 divisors_dict[x] = divisors_list
-        
+        if 1 <= (len(divisors_list)) <= 4:
+            t.add_row([ '', divisors_list, ''])
+        elif (len(divisors_list)) == 0:
+            t.add_row([ '', '...', ''])
+        else:
+            while len(divisors_list) > 5:
+                t.add_row([ '', divisors_list[:4], ''])
+                divisors_list = divisors_list[4:]
+            t.add_row([ '', divisors_list[:4], ''])
+        t.add_row([ '-', '-', num_divisors])
+        t.add_row(['------','-----------------','------'])
         num_divisors_dict[x] = num_divisors
-        print('number %d has %d divisors' % (x, num_divisors))
+        #print('number %d has %d divisors' % (x, num_divisors))
         
             
-    print(divisors_dict, 'Number of divisors', num_divisors_dict)
+    #print(divisors_dict, 'Number of divisors', num_divisors_dict)
+    print (t)
     return divisors_dict, num_divisors_dict
 
     '''lets find the max value from the interval, which has least
@@ -52,28 +65,33 @@ def max_num_least(a, b):
     master = []
     num_div = []
     values = []
+    #p = PrettyTable(["Number", "Divisors", "no Divisors"])
+    #p.padding_width = 1
     for x in range(a, b + 1):
         output = []
         num_divisors = 0
         output.append(x)
         values.append(x)
+        #p.add_row([x, '-', '-'])
         for y in range(2, x + 1):
             if x % y == 0 and x != y:
                 num_divisors += 1
-                    
-
+                #p.add_row([ '', y, ''])    
+        #p.add_row([ '-', '-', num_divisors])
+        #p.add_row(['------','------','------'])
         if num_divisors >= 1:
             num_div.append(num_divisors)
         output.append(num_divisors)
         master.append(output)
-    print (master)
+    #print (p)
+    #print (master)
     #print (values)
     #print (num_div)
     '''
     note that i skipped zeros in num_divisors since we dont take values without divisors into account
     as soon as i get all three lists filled we can start analyzing:
     '''
-  
+    
     val_min_div = []    
     for i in range(len(master)):
         if master[i][1] == min(num_div):
